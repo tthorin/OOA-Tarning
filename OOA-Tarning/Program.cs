960e64e9fe61c1dcf5fab5d26049a4db7919ca62
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-11-02 10:54:34 +0100

    added csharp project

---
+++ OOA-Tarning/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOA_Tarning
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
