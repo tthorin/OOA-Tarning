456c5e1ec249674f1fa9c3a1a7db4a57b41ff267
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-11-02 11:02:15 +0100

    added Die class

---
+++ OOA-Tarning/Die.cs
@@ -0,0 +1,18 @@
+﻿namespace OOA_Tarning
+{
+    using System;
+    public class Die
+    {
+        static Random rng = new();
+        private int sides = 6;
+        public Die(int sides = 6)
+        {
+            this.sides = sides;
+        }
+
+        public int Roll()
+        {
+            return rng.Next(1, sides + 1);
+        }
+    }
+}
