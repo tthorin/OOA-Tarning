de15fbac3a07882185fa1bb134e1b2c2cc35508e
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-05 10:16:20 +0100

    more fix

--- OOA-Tarning/Die.cs
+++ OOA-Tarning/Die.cs
@@ -23,33 +23,23 @@
         }
         public int PrintDice(string whoIsRolling)
         {   
-            int diceHeight = 5;
             int diceResult = rng.Next(1, sides + 1);
 
-            for (int diceRow = 0; diceRow < diceHeight; diceRow++)
-            {
+            Console.WriteLine("".PadRight(20) + "┌─────┐");
 
-                if (diceRow == 0) Console.WriteLine("".PadRight(20)+"┌─────┐");
-                if (diceRow == 1)
-                {
-                    if (diceResult == 1) Console.WriteLine("".PadRight(20) + "│     │");
-                    else if (diceResult == 2 || diceResult == 3) Console.WriteLine("".PadRight(20) + "│o    │");
-                    else Console.WriteLine("".PadRight(20) + "│o   o│");
-                }
-                if (diceRow == 2)
-                {
-                    if (diceResult == 4 || diceResult == 2) Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│     │");
-                    else if (diceResult == 6) Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│o   o│");
-                    else Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│  o  │");
-                }
-                if (diceRow == 3)
-                {
-                    if (diceResult == 3 || diceResult == 2) Console.WriteLine("".PadRight(20) + "│    o│");
-                    else if (diceResult == 1) Console.WriteLine("".PadRight(15) + "│     │");
-                    else Console.WriteLine("".PadRight(20) + "│o   o│");
-                }
-                if (diceRow == 4) Console.WriteLine("".PadRight(20) + "└─────┘");
-            }
+            if (diceResult == 1) Console.WriteLine("".PadRight(20) + "│     │");
+            else if (diceResult == 2 || diceResult == 3) Console.WriteLine("".PadRight(20) + "│o    │");
+            else Console.WriteLine("".PadRight(20) + "│o   o│");
+
+            if (diceResult == 4 || diceResult == 2) Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│     │");
+            else if (diceResult == 6) Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│o   o│");
+            else Console.WriteLine($"{whoIsRolling} rolls:".PadRight(20) + "│  o  │");
+
+            if (diceResult == 3 || diceResult == 2) Console.WriteLine("".PadRight(20) + "│    o│");
+            else if (diceResult == 1) Console.WriteLine("".PadRight(20) + "│     │");
+            else Console.WriteLine("".PadRight(20) + "│o   o│");
+
+            Console.WriteLine("".PadRight(20) + "└─────┘");
 
             return diceResult;
         }
